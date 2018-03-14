0a803e799c71498a57151601c6ae201b0968b69d
Jesse Mavi <jesse.mavi@gmail.com>
2018-03-14 16:52:03 -0500

    Adds user login route

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -28,4 +28,33 @@ userRouter.post('/add-user', async (req, res) => {
   }
 });
 
+// login a user
+userRouter.post('/login', async (req, res) => {
+  try {
+    // get user from db
+    const query = await db.query(`select * from users where email = '${req.body.email}'`);
+    const user = query.rows[0];
+    console.log('user', user);
+
+    if(!user) {
+      return res.send({error: 'email does not exist'});
+    }
+
+    // validate password with hashed password
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    console.log('validPassword', validPassword);
+
+    if(!validPassword) {
+      return res.send({error: 'wrong password'});
+    }
+
+    // create token to send back in response
+    const token = await jwt.sign({id: user.id}, 'interview-secret-1', {expiresIn: '10hr'});
+    res.send({user_id: user.id, token: token});
+  } catch(err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
 module.exports = userRouter;
